5b9602ea45bed8d3ef067c3cfbb0a04d76373e85
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-08-13 15:56:32 +0700

    feat: create jwt.js

---
+++ helpers/jwt.js
@@ -0,0 +1,11 @@
+const jwt = require("jsonwebtoken");
+
+function signToken(payload) {
+  return jwt.sign(payload, JWT_SECRET);
+}
+
+function verifyToken(token) {
+  return jwt.verify(token, JWT_SECRET);
+}
+
+module.exports = { signToken, verifyToken };
