ff00708178d0b2738653a5ce6f8aef9ab1966d47
mnaufaldillah <mnaufaldillah@gmail.com>
2024-08-13 16:37:58 +0700

    Models, Migrations, and Seeders

---
+++ seeders/20240813091248-seed-User.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    const dataUser = JSON.parse(await fs.readFile(`./data/users.json`, `utf8`)).map((item) => {
+      delete item.id;
+      item.createdAt = item.updatedAt = new Date();
+      return item
+     });
+  
+     await queryInterface.bulkInsert(`Users`, dataUser, {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
