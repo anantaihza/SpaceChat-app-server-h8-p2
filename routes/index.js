ca25ff27cba51688140242f8af6b3cbd502eccab
MuhammadFarhanRosidi <muhfarros28@gmail.com>
2024-08-14 11:13:12 +0700

    feat: create routes/profile.js, add env-template cloudinary, create updateProfile controller, import Op sequelize, create route login register and profile

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
+const UserController = require("../controllers/UserController");
 const authentication = require("../middlewares/authentication");
 const router = require(`express`).Router();
 const routerGroup = require(`./groups`);
 const routerMyGroup = require(`./myGroups`);
+const routerProfile = require(`./profile`);
+
+router.post(`/register`, UserController.register);
+router.post(`/login`, UserController.login);
 
 router.use(authentication);
 
 router.use(`/groups`, routerGroup);
 router.use(`/myGroups`, routerMyGroup);
+router.use(`/profile`, routerProfile);
 
 module.exports = router;
